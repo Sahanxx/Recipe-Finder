45d87b46e9a97513672fa2f700fb2b2b6958e399
Sahan Anjana Hewage <sahananjana63@gmail.com>
2025-09-24 13:22:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // Must match your repo name exactly (case-sensitive)
+  base: '/Recipe-Finder/',
+});
